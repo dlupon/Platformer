a4ebdd2e050282956c695c282410b64518951d06
unbocal <dylan1lupon@gmail.com>
2024-05-01 15:04:28 +0200

    First Comit
    Initialized the project.

---
+++ Scripts/Main.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+// Author	: Dylan Lupon
+// Date		: 05/01/2024
+namespace Com.IsartDigital.Platformer
+{
+	
+	public class Main : Node
+	{
+		// ----------------~~~~~~~~~~~~~~~~~~~==========================# // Variables
+
+		
+
+		// ----------------~~~~~~~~~~~~~~~~~~~==========================# // Initialization
+
+		public override void _Ready()
+		{
+			GD.Print("Hello World");
+		}
+	}
+}
